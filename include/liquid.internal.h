8141bb941f351fedb9a7fdd81bc3015ba23a11be
João Silva <jgc3silva@gmail.com>
2021-11-02 16:30:17 +0000

    Revert "reverting fec changes from recent merge until proper CI integration"
    This reverts commit 53bf7a971777a657c2e85c8133e2c6e74b16fbc4.

--- include/liquid.internal.h
+++ include/liquid.internal.h
@@ -101,7 +101,7 @@ struct fec_s {
 
     // viterbi decoder function pointers
     void*(*create_viterbi)(int);
-    //void (*set_viterbi_polynomial)(int*);
+    void (*set_viterbi_polynomial)(int*);
     int  (*init_viterbi)(void*,int);
     int  (*update_viterbi_blk)(void*,unsigned char*,int);
     int  (*chainback_viterbi)(void*,unsigned char*,unsigned int,unsigned int);
