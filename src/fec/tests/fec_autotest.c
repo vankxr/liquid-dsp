e80865dbbc68c755cfee4963acdc19df591c308d
João Silva <jgc3silva@gmail.com>
2024-05-19 01:24:27 +0100

    Add autotests for the three new RS FEC schemes
    Signed-off-by: João Silva <jgc3silva@gmail.com>

--- src/fec/tests/fec_autotest.c
+++ src/fec/tests/fec_autotest.c
@@ -90,44 +90,47 @@ void fec_test_codec(fec_scheme _fs, unsigned int _n, void * _opts)
 //
 
 // repeat codes
-void autotest_fec_r3()      { fec_test_codec(LIQUID_FEC_REP3,          64, NULL); }
-void autotest_fec_r5()      { fec_test_codec(LIQUID_FEC_REP5,          64, NULL); }
+void autotest_fec_r3()             { fec_test_codec(LIQUID_FEC_REP3,              64, NULL); }
+void autotest_fec_r5()             { fec_test_codec(LIQUID_FEC_REP5,              64, NULL); }
 
 // Hamming block codes
-void autotest_fec_h74()     { fec_test_codec(LIQUID_FEC_HAMMING74,     64, NULL); }
-void autotest_fec_h84()     { fec_test_codec(LIQUID_FEC_HAMMING84,     64, NULL); }
-void autotest_fec_h128()    { fec_test_codec(LIQUID_FEC_HAMMING128,    64, NULL); }
+void autotest_fec_h74()            { fec_test_codec(LIQUID_FEC_HAMMING74,         64, NULL); }
+void autotest_fec_h84()            { fec_test_codec(LIQUID_FEC_HAMMING84,         64, NULL); }
+void autotest_fec_h128()           { fec_test_codec(LIQUID_FEC_HAMMING128,        64, NULL); }
 
 // Golay block codes
-void autotest_fec_g2412()   { fec_test_codec(LIQUID_FEC_GOLAY2412,     64, NULL); }
+void autotest_fec_g2412()          { fec_test_codec(LIQUID_FEC_GOLAY2412,         64, NULL); }
 
 // SEC-DED block codecs
-void autotest_fec_secded2216() { fec_test_codec(LIQUID_FEC_SECDED2216, 64, NULL); }
-void autotest_fec_secded3932() { fec_test_codec(LIQUID_FEC_SECDED3932, 64, NULL); }
-void autotest_fec_secded7264() { fec_test_codec(LIQUID_FEC_SECDED7264, 64, NULL); }
+void autotest_fec_secded2216()     { fec_test_codec(LIQUID_FEC_SECDED2216,     64, NULL); }
+void autotest_fec_secded3932()     { fec_test_codec(LIQUID_FEC_SECDED3932,     64, NULL); }
+void autotest_fec_secded7264()     { fec_test_codec(LIQUID_FEC_SECDED7264,     64, NULL); }
 
 // convolutional codes
-void autotest_fec_v27()     { fec_test_codec(LIQUID_FEC_CONV_V27,      64, NULL); }
-void autotest_fec_v29()     { fec_test_codec(LIQUID_FEC_CONV_V29,      64, NULL); }
-void autotest_fec_v39()     { fec_test_codec(LIQUID_FEC_CONV_V39,      64, NULL); }
-void autotest_fec_v615()    { fec_test_codec(LIQUID_FEC_CONV_V615,     64, NULL); }
+void autotest_fec_v27()            { fec_test_codec(LIQUID_FEC_CONV_V27,          64, NULL); }
+void autotest_fec_v29()            { fec_test_codec(LIQUID_FEC_CONV_V29,          64, NULL); }
+void autotest_fec_v39()            { fec_test_codec(LIQUID_FEC_CONV_V39,          64, NULL); }
+void autotest_fec_v615()           { fec_test_codec(LIQUID_FEC_CONV_V615,         64, NULL); }
 
 // convolutional codes (punctured)
-void autotest_fec_v27p23()  { fec_test_codec(LIQUID_FEC_CONV_V27P23,   64, NULL); }
-void autotest_fec_v27p34()  { fec_test_codec(LIQUID_FEC_CONV_V27P34,   64, NULL); }
-void autotest_fec_v27p45()  { fec_test_codec(LIQUID_FEC_CONV_V27P45,   64, NULL); }
-void autotest_fec_v27p56()  { fec_test_codec(LIQUID_FEC_CONV_V27P56,   64, NULL); }
-void autotest_fec_v27p67()  { fec_test_codec(LIQUID_FEC_CONV_V27P67,   64, NULL); }
-void autotest_fec_v27p78()  { fec_test_codec(LIQUID_FEC_CONV_V27P78,   64, NULL); }
+void autotest_fec_v27p23()         { fec_test_codec(LIQUID_FEC_CONV_V27P23,       64, NULL); }
+void autotest_fec_v27p34()         { fec_test_codec(LIQUID_FEC_CONV_V27P34,       64, NULL); }
+void autotest_fec_v27p45()         { fec_test_codec(LIQUID_FEC_CONV_V27P45,       64, NULL); }
+void autotest_fec_v27p56()         { fec_test_codec(LIQUID_FEC_CONV_V27P56,       64, NULL); }
+void autotest_fec_v27p67()         { fec_test_codec(LIQUID_FEC_CONV_V27P67,       64, NULL); }
+void autotest_fec_v27p78()         { fec_test_codec(LIQUID_FEC_CONV_V27P78,       64, NULL); }
 
-void autotest_fec_v29p23()  { fec_test_codec(LIQUID_FEC_CONV_V29P23,   64, NULL); }
-void autotest_fec_v29p34()  { fec_test_codec(LIQUID_FEC_CONV_V29P34,   64, NULL); }
-void autotest_fec_v29p45()  { fec_test_codec(LIQUID_FEC_CONV_V29P45,   64, NULL); }
-void autotest_fec_v29p56()  { fec_test_codec(LIQUID_FEC_CONV_V29P56,   64, NULL); }
-void autotest_fec_v29p67()  { fec_test_codec(LIQUID_FEC_CONV_V29P67,   64, NULL); }
-void autotest_fec_v29p78()  { fec_test_codec(LIQUID_FEC_CONV_V29P78,   64, NULL); }
+void autotest_fec_v29p23()         { fec_test_codec(LIQUID_FEC_CONV_V29P23,       64, NULL); }
+void autotest_fec_v29p34()         { fec_test_codec(LIQUID_FEC_CONV_V29P34,       64, NULL); }
+void autotest_fec_v29p45()         { fec_test_codec(LIQUID_FEC_CONV_V29P45,       64, NULL); }
+void autotest_fec_v29p56()         { fec_test_codec(LIQUID_FEC_CONV_V29P56,       64, NULL); }
+void autotest_fec_v29p67()         { fec_test_codec(LIQUID_FEC_CONV_V29P67,       64, NULL); }
+void autotest_fec_v29p78()         { fec_test_codec(LIQUID_FEC_CONV_V29P78,       64, NULL); }
 
 // Reed-Solomon block codes
-void autotest_fec_rs8()     { fec_test_codec(LIQUID_FEC_RS_M8,         64, NULL); }
+void autotest_fec_rs8()            { fec_test_codec(LIQUID_FEC_RS_M8,             64, NULL); }
+void autotest_fec_rs8_k223_ccsds() { fec_test_codec(LIQUID_FEC_RS_M8_K223_CCSDS,  64, NULL); }
+void autotest_fec_rs8_k239_ccsds() { fec_test_codec(LIQUID_FEC_RS_M8_K239_CCSDS,  64, NULL); }
+void autotest_fec_rs8_dvb()        { fec_test_codec(LIQUID_FEC_RS_M8_DVB,         64, NULL); }
 
 
